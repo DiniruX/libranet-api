21a9368f2eb4dca4c3a7712a7e6ab5e1d3b2a5e4
Diniru Sandipa <it20041120@my.sliit.lk>
2025-05-31 21:25:43 +0900

    new endpoints implemented for inter library reservations

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.api import test, library, user, book, reservation
+from app.api import test, library, user, book, reservation, interLibReservations
 
 app = FastAPI()
 app.include_router(test.router)
@@ -7,3 +7,4 @@ app.include_router(library.router)
 app.include_router(user.router)
 app.include_router(book.router)
 app.include_router(reservation.router)
+app.include_router(interLibReservations.router)
